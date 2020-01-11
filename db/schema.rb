a68f93a4a482f4b9a41d88e25e5654bcf7aa86a4
Kostas <spitha3@hotmail.com>
2020-01-11 16:16:53 +0200

    Self-Ref Friendship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_09_221847) do
+ActiveRecord::Schema.define(version: 2020_01_11_133511) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,7 +27,9 @@ ActiveRecord::Schema.define(version: 2020_01_09_221847) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "user_ID"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
