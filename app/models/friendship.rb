577242b3639c7e5c26d75069f14b237cfc90e0a7
Kostas <spitha3@hotmail.com>
2020-01-12 16:02:51 +0200

    friendship validations
     user cannot befriend himself
     user cannot befriend a friend

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,14 @@
 class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, :class_name => "User"
+  validate :not_self
+  validate :not_friends
+
+  def not_self
+  errors.add(:friend, "can't be equal to user") if user == friend
+  end
+  def not_friends
+    errors.add(:friend, 'is already added') if user.friends.include?(friend)
+  end
+
 end
