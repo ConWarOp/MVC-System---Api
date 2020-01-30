f196c6e91dadda462fbd57220cdf05cb8e62f6a9
dimitra <p16178@students.cs.unipi.gr>
2020-01-30 23:01:13 +0200

    login with facebook
    done - no more fixes

--- app/controllers/callbacks_controller.rb
+++
@@ -1,6 +0,0 @@
-class CallbacksController < ApplicationController
-  def facebook
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-    sign_in_and_redirect_to root_url @user
-  end
-end
