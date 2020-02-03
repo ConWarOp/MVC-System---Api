9ae8fc93d684931ab87c241e0428684342ec6997
Kostas <spitha3@hotmail.com>
2020-02-03 14:55:48 +0200

    psql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_02_03_101053) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
     t.integer "friend_id"
@@ -31,7 +34,7 @@ ActiveRecord::Schema.define(version: 2020_02_03_101053) do
     t.string "title"
     t.text "content"
     t.text "category"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
