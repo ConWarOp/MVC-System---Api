f558ad8cb5b3a7f9e5bac85ee9e3ce43248e32d3
dimitra <p16178@students.cs.unipi.gr>
2020-02-05 18:15:07 +0200

    Final project
    works fine on localhost including chat

--- config/initializers/omniauth.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider "1060558194293395", "4c81e971a6f9622e50d41809c5c7d2eb",
-           {:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
-end
